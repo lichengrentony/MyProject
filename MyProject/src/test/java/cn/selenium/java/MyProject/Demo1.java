3dcbe8318d2ba71d4eb27f69e0778a881424c119
lichengren <lichengrentony@sina.com>
2019-11-26 08:23:33 +0800

    update 20191126

--- MyProject/src/test/java/cn/selenium/java/MyProject/Demo1.java
+++ MyProject/src/test/java/cn/selenium/java/MyProject/Demo1.java
@@ -15,31 +15,9 @@ public class Demo1 extends Feature{
 		
 		getUrl();
 
-		System.out.println(isEnable(findElementById("searchkey")));
-
-		clearAndSendKeys(findElementById("searchkey"),"selenium");
-
-		System.out.println(getTitle());
-
-		System.out.println(getCurrentUrl());
-
-		Navigate("http://www.baidu.com");
-
-		goBack();
-
-		goForward();
-
-		refresh();
-
-		System.out.println(getPageSource());
-
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+		for (WebElement webelement:findElements("//div[@id=\"u1\"]//a")) {
+			webelement.click();
 		}
-
-
 	}
 
 	@BeforeMethod
