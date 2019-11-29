f569e9e851e48446c52136e72db57e857721112e
tony <lichengrentony@sina.com>
2019-11-29 18:00:13 +0800

    Update 20191129 6pm

--- MyProject/src/main/java/cn/selenium/java/MyProject/Feature.java
+++ MyProject/src/main/java/cn/selenium/java/MyProject/Feature.java
@@ -1,6 +1,5 @@
 package cn.selenium.java.MyProject;
 
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,7 +11,6 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import sun.reflect.misc.FieldUtil;
 
 import java.io.File;
 import java.io.IOException;
--- MyProject/src/test/java/cn/selenium/java/MyProject/Demo1.java
+++ MyProject/src/test/java/cn/selenium/java/MyProject/Demo1.java
@@ -1,36 +1,32 @@
 package cn.selenium.java.MyProject;
 
-import org.openqa.selenium.Keys;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.sikuli.script.FindFailed;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.awt.*;
 import java.io.IOException;
 
 public class Demo1 extends Feature {
 
-    @Test
-    public void f() throws InterruptedException, IOException {
+    public Logger log = LogManager.getLogger(Demo1.class.getName());
 
-        killAllBrowser();
+    @Test
+    public void f() throws InterruptedException, IOException, AWTException, FindFailed {
 
         getUrl();
-
-        setPosition(150,150);
-
-        setDimension(500,500);
-
-        getPosition();
-
-        getSize();
-
-        Thread.sleep(3000);
-
+        log.debug("Debug message logged");
+        log.error("Error message logged");
+        log.fatal("fatal message logged");
     }
 
     @BeforeMethod
     public void beforeMethod() {
-        before("chrome");
+        before("ie");
     }
 
     @AfterMethod
